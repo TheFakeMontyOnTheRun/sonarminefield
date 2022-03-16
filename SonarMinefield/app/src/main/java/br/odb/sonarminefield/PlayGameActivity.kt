a112e3720107802b0f507f388b5554c5a7ecf64e
Daniel Monteiro <monteiroquiet@gmail.com>
2022-03-16 21:43:11 +0000

    Make game fullscreen

--- SonarMinefield/app/src/main/java/br/odb/sonarminefield/PlayGameActivity.kt
+++ SonarMinefield/app/src/main/java/br/odb/sonarminefield/PlayGameActivity.kt
@@ -1,12 +1,17 @@
 package br.odb.sonarminefield
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 
 class PlayGameActivity : AppCompatActivity() {
 
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.decorView.systemUiVisibility =
+            View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY or View.SYSTEM_UI_FLAG_FULLSCREEN or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+
         setContentView(R.layout.activity_game_layout)
     }
 }
