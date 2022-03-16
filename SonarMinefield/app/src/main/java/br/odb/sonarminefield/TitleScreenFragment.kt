5a41e5dd24c3e95e534886fa04ee28edcc43ff07
Daniel Monteiro <monteiroquiet@gmail.com>
2022-03-16 21:41:27 +0000

    Add a better title screen
    Not the final form, but fully functional and much better looking than a single button.

--- SonarMinefield/app/src/main/java/br/odb/sonarminefield/TitleScreenFragment.kt
+++ SonarMinefield/app/src/main/java/br/odb/sonarminefield/TitleScreenFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SeekBar
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
@@ -25,8 +26,21 @@ class TitleScreenFragment : Fragment() {
             false
         )
 //        viewModel = ViewModelProvider(this).get(TitleScreenViewModel::class.java)
+
+        binding.skMines.setOnSeekBarChangeListener( object : SeekBar.OnSeekBarChangeListener  {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                binding.tvMines.text = "Playing with ${binding.skMines.progress + 20 } mines"
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+            }
+        })
+
         binding.btnPlay.setOnClickListener {
-            findNavController().navigate(TitleScreenFragmentDirections.actionTitleScreenFragmentToBoardFragment(27, 50))
+            findNavController().navigate(TitleScreenFragmentDirections.actionTitleScreenFragmentToBoardFragment(binding.skMines.progress + 20, 50))
         }
 
         return binding.root
